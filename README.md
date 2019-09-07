128a81d2fa76e135ebbad62e74a822965ff42f28
mulic3 <mulic3@gmail.com>
2019-09-07 23:44:28 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# StarWars API Fetcher
+## A simple API fetcher implementing ES6's Fetch API.
+This snippet will help you recognize the necessary steps for fetching data from local/external API's using ES6's Fetch.
---
+++ script.js
@@ -0,0 +1,58 @@
+    document.getElementById('getText').addEventListener('click', getText);
+    document.getElementById('getUsers').addEventListener('click', getUsers);
+    document.getElementById('getPosts').addEventListener('click', getPosts);
+
+    function getText() {
+        fetch('sample.txt')
+        .then(function(res) {
+            return res.text();
+        })
+        .then(function(data) {
+           document.getElementById('output').innerHTML = data;
+        })
+        .catch(function(err) {
+            console.log(err);
+        })
+       document.getElementsByTagName("IMG")[0].style.display = 'none';
+    }
+
+    function getUsers() {
+        fetch('users.json')
+        .then(function(res) {
+            return res.json()
+        })
+        .then(function(data) {
+           var output = '<h2 class="mb-4">Star-Wars Users</h2>' 
+           data.forEach(function(user) {
+               output += `
+               <ul class="list-group mb-4">
+                <li class="list-group-item">ID: ${user.id}</li>
+                <li class="list-group-item">Name: ${user.name}</li>
+                <li class="list-group-item">Email: ${user.email}</li>
+               </ul>
+               `;
+           });
+        document.getElementById('output').innerHTML = output;
+        });
+       document.getElementsByTagName("IMG")[0].style.display = 'none';
+    }    
+
+    function getPosts() {
+        fetch('https://swapi.co/api/films')
+        .then(function(res) {
+            return res.json()
+        })
+        .then(function(data) {
+           var output = '<h2 class="mb-4">Star-Wars Movies\' Plots</h2>' 
+           data.results.forEach(function(film) {
+               output += `
+               <div class="card card-body mb-4">
+                <h3>Title: ${film.title}</h3>
+                <p>Post: ${film.opening_crawl}</p>
+               </div>
+               `;
+           });
+        document.getElementById('output').innerHTML = output;
+        });
+       document.getElementsByTagName("IMG")[0].style.display = 'none';
+    }  
